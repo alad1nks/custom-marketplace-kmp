14b6a5b7e9555eb62505f80bc8e8da15b7426e49
alad1nks <vasya.sidr777@gmail.com>
2026-03-14 18:41:01 +0300

    Add lint

--- composeApp/src/jvmMain/kotlin/com/alad1nks/custommarketplace/main.kt
+++ composeApp/src/jvmMain/kotlin/com/alad1nks/custommarketplace/main.kt
@@ -3,11 +3,12 @@ package com.alad1nks.custommarketplace
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
-fun main() = application {
-    Window(
-        onCloseRequest = ::exitApplication,
-        title = "custommarketplace",
-    ) {
-        App()
+fun main() =
+    application {
+        Window(
+            onCloseRequest = ::exitApplication,
+            title = "custommarketplace",
+        ) {
+            App()
+        }
     }
-}
--- composeApp/src/wasmJsMain/kotlin/com/alad1nks/custommarketplace/Platform.wasmJs.kt
+++ composeApp/src/wasmJsMain/kotlin/com/alad1nks/custommarketplace/Platform.wasmJs.kt
@@ -1,6 +1,6 @@
 package com.alad1nks.custommarketplace
 
-class WasmPlatform: Platform {
+class WasmPlatform : Platform {
     override val name: String = "Web with Kotlin/Wasm"
 }
 
